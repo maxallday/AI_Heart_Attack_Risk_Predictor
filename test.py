cd4bab55b7062f67c8fa2e0fd7fe56477a5e4bf3
maxallday <modiga58@gmail.com>
2025-06-19 08:09:41 +0300

    update

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+'''import tensorflow as tf
 print("TensorFlow version:", tf.__version__)
 print("Devices:", tf.config.list_physical_devices('GPU'))
 from tensorflow import keras
+'''
